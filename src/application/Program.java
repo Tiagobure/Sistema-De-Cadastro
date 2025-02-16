0d0b4c4ddac05df97d29507d2b7dd38e2fe327e4
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-16 00:08:21 -0300

    fix: Passo 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,33 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       
+		List<String> list =new ArrayList<>();
+		String path = "c:\\teste\\arquivo.txt";
+		
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String question = br.readLine();
+			while(question != null) {
+				list.add(question);
+				question = br.readLine();
+			}
+			for(String element : list) {
+				System.out.println(element);
+			}
+			
+		} catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
 	}
 
 }
--- src/application/model/Cadastro.java
+++ src/application/model/Cadastro.java
@@ -1,5 +1,54 @@
 package application.model;
 
 public class Cadastro {
+	
+	private String name;
+	private String email;
+	private Integer age;
+	private Double height;
+	
+	public Cadastro(String name, String email, Integer age, Double height) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
+		this.height = height;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+	
+	
+	
+	
+
 
 }
